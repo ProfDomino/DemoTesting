1f36c880ea31a0d92eb741fe6dcc75d99d75d88d
Will Turnage <will@turnage.co>
2024-05-29 10:58:49 -0400

    Fixed build error

--- app/[stateAbbreviation]/book/[bookId]/page.tsx
+++ app/[stateAbbreviation]/book/[bookId]/page.tsx
@@ -8,7 +8,6 @@ import { BookDetail } from "@/components/books/book-detail";
 import { BookTopics } from "@/components/books/book-topics";
 import { Book, BookPreview } from "@/types/book";
 import { BookMeta } from "@/types/book-meta";
-import data from "@/data/msba_books.json";
 
 interface BookData {
   browse: BookPreview[];
@@ -22,9 +21,88 @@ export default function BookDetailPage({
   params: { bookId: string };
 }): JSX.Element {
   const { bookId = "" } = params;
-  const bookData = data as BookData;
-  const book: Book | undefined = bookData.books[bookId];
-  const bookMetum: BookMeta | undefined = bookData.bookMeta[bookId];
+  const book: Book = {
+    isbn10: "059356619X",
+    isbn13: "9780593566190",
+    title: "Something Like Home",
+    image: "https://cdn.remo.app/images/56/19/9780593566190.jpg",
+    pageCount: "248",
+    wordCount: "0",
+    pubDate: "2023",
+    copyrightDate: "2023",
+    foreword: "",
+    edition: "0",
+    format: "",
+    isUnpaged: false,
+    _id: "65ff1fb51c5f682b7e242a56",
+  };
+
+  const bookMetum: BookMeta = {
+    _id: "65ff1fb51c5f682b7e242a56",
+    title: "Something Like Home",
+    subtitle: "",
+    author: "Andrea Beatriz Arango",
+
+    editor: "",
+    illustrator: "",
+    creators: [],
+    synopsis:
+      "Laura grapples with the decision to call 911 after finding her parents, fearing that life would have remained unchanged if she hadn't taken action. Instead, she finds herself in kinship care with a relative she barely knows, leading to uncertainty about her future. When Laura rescues an abandoned dog and gains permission to keep it, she discovers the concept of therapy dogs and decides to train her pet. Through this journey, she forms a new friendship and gains insight into the challenges others face.",
+    publisher: ["Random House Books for Young Readers"],
+    characterEthnicity: [],
+    characterGenderIdentity: [],
+    characterRaceCulture: [],
+    characterReligion: [],
+    characterSexualOrientation: [],
+    awards: [],
+    contentWarning: [],
+    timePeriod: [],
+    narrativeForm: ["Novels-In-Verse"],
+    genre: ["Fiction: Realistic"],
+    historicalEvents: [],
+    internationalAwards: [],
+    language: [],
+    languageRegister: [],
+    literaryDevices: [],
+    modesOfWriting: [],
+    pointOfView: [],
+    subject: [],
+    textFeatures: [],
+    textStructure: [],
+    topic: ["Family Situations"],
+    voice: [],
+    tags: ["dogs"],
+    isFiction: true,
+    isNonFiction: false,
+    isBlended: false,
+    guidedReadingLevel: "",
+    lexileLevel: "",
+    hasMultiplePov: false,
+    hasUnreliableNarrative: false,
+    isbn10: ["059356619X"],
+    isbn13: ["9780593566190"],
+    series: "",
+    seriesBookNumber: 1,
+    seriesType: "Stand Alone",
+    defaultEdition: 0,
+    editions: [
+      {
+        isbn10: "059356619X",
+        isbn13: "9780593566190",
+        title: "Something Like Home",
+        author: "Andrea Beatriz Arango",
+        image: "https://cdn.remo.app/images/56/19/9780593566190.jpg",
+        pageCount: "248",
+        wordCount: "0",
+        pubDate: "2023",
+        copyrightDate: "2023",
+        foreword: "",
+        edition: "0",
+        format: "",
+        isUnpaged: false,
+      },
+    ],
+  };
 
   return book && bookMetum ? (
     <Box>
